42e6ff8839dc13307777771d73718b047d535521
PapitaConPure <33335038+PapitaConPure@users.noreply.github.com>
2023-10-15 19:22:40 -0300

    Actualizar Hotel y Habitación

---
+++ tp2/AlquilerLib/Habitación.cs
@@ -0,0 +1,28 @@
+﻿namespace AlquilerLib {
+	class Habitación {
+		private TipoHabitación tipo;
+
+		public enum TipoHabitación {
+			Simple = 0,
+			Doble,
+			Triple,
+		}
+
+		public Habitación(int número, TipoHabitación tipo) {
+			this.Número = número;
+			this.tipo = tipo;
+		}
+
+		public int Número { get; private set; }
+
+		public double PcntAdicional {
+			get {
+				switch(this.tipo) {
+				case TipoHabitación.Doble:  return 0.8;
+				case TipoHabitación.Triple: return 1.5;
+				default: return 0;
+				}
+			}
+		}
+	}
+}
--- tp2/AlquilerLib/Hotel.cs
+++ tp2/AlquilerLib/Hotel.cs
@@ -6,18 +6,48 @@ using System.Threading.Tasks;
 
 namespace AlquilerLib {
 	class Hotel: Propiedad {
+		private int estrellas;
+		private List<Habitación> habitaciones;
 
+		public Hotel(int estrellas) {
+			this.estrellas = estrellas;
+			this.habitaciones = new List<Habitación>();
+		}
+
+		public void AgregarHabitación(int número, Habitación.TipoHabitación tipo) {
+			this.habitaciones.Add(new Habitación(número, tipo));
+		}
+
+		public void QuitarHabitación(int número) {
+			this.habitaciones.Remove(this.VerHabitación(número));
+		}
+
+		public double PrecioPorDía(int número) {
+			double precio = this.precioBase;
+
+			precio += precio * this.VerHabitación(número).PcntAdicional;
+
+			if(this.estrellas >= 3)
+				precio += precio * 0.4;
+
+			return precio;
+		}
+
+		private Habitación VerHabitación(int número) {
+			Habitación encontrada = null;
+			int c = 0;
+
+			while(c < this.habitaciones.Count && encontrada == null) {
+				if(this.habitaciones[c].Número == número)
+					encontrada = this.habitaciones[c];
+				c++;
+			}
+
+			return encontrada;
+		}
 
 		public override string Imprimir() {
 			return "";
-		}
-	}
-
-	class Habitación {
-		public enum TipoHabitación {
-			Simple,
-			Doble,
-			Triple,
 		}
 	}
 }
