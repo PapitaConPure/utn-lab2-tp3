1cad2eda6a20d5fddd4b4307c32f1ee951fb4295
euurodri <111576327+euurodri@users.noreply.github.com>
2023-10-16 21:14:22 -0300

    Cambios

--- tp2/tp2/FPrincipal.cs
+++ tp2/tp2/FPrincipal.cs
@@ -13,15 +13,15 @@ using AlquilerLib;
 
 namespace tp2 {
 	public partial class FPrincipal: Form {
-		
+		Sistema sistema;
         public FPrincipal() {
 			this.InitializeComponent();
 		}
 		private void FPrincipal_Load(object sender, EventArgs e) {
 			this.ofdElegirImagen.InitialDirectory = Environment.CurrentDirectory;
-
 			this.mcReservas.MinDate = DateTime.Now;
 			this.mcReservas.MaxDate = DateTime.Now.AddMonths(3);
+			sistema = new Sistema();
 		}
 
 		private void SeleccionarTextBox(object sender, EventArgs e) {
@@ -45,18 +45,63 @@ namespace tp2 {
 		}
 
 		private void BtnConsultarAlquiler_Click(object sender, EventArgs e) {
-			
+			if (sistema.ConsultarPropiedad(Convert.ToInt32(nudNroAlquiler.Value)) != null)
+			{
+				MessageBox.Show("La propiedad está disponible.");
+			}
+			else
+			{
+				MessageBox.Show("La propiedad no está disponible.");
+			}
+
 		}
 
-		private List<string>Servicios() {
-			List<string> servicios = new List<string>();
-			if (cbCochera.Checked)servicios.Add("Cochera");
-			if (cbDesayuno.Checked) servicios.Add("Desayuno");
-			if (cbLimpieza.Checked) servicios.Add("Limpieza");
-			if (cbMascotas.Checked) servicios.Add("Mascotas");
-			if (cbPileta.Checked) servicios.Add("Pileta");
-			if (cbWifi.Checked) servicios.Add("Wi Fi");
-			return servicios;
-		}
-	}
+
+        private void btnCancelarAlquiler_Click(object sender, EventArgs e)
+        {
+			if(sistema.CancelarAlquiler(Convert.ToInt32(nudNumPropiedad), Convert.ToInt32(nudNroAlquiler.Value))) MessageBox.Show("El alquiler fue cancelado con éxito.");
+			
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        private List<string> Servicios()
+        {
+            List<string> servicios = new List<string>();
+            if (cbCochera.Checked) servicios.Add("Cochera");
+            if (cbDesayuno.Checked) servicios.Add("Desayuno");
+            if (cbLimpieza.Checked) servicios.Add("Limpieza");
+            if (cbMascotas.Checked) servicios.Add("Mascotas");
+            if (cbPileta.Checked) servicios.Add("Pileta");
+            if (cbWifi.Checked) servicios.Add("Wi Fi");
+            return servicios;
+        }
+    }
 }
