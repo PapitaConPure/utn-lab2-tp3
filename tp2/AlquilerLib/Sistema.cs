19456a7af44d4b1597bb11a96ff3a42066e39687
PapitaConPure <33335038+PapitaConPure@users.noreply.github.com>
2023-10-16 21:13:54 -0300

    Mejorar devolución de métodos y documentar

--- tp2/AlquilerLib/Sistema.cs
+++ tp2/AlquilerLib/Sistema.cs
@@ -13,6 +13,10 @@ namespace AlquilerLib {
 			this.propiedades = new List<Propiedad>();
 		}
 
+		public void AgregarPropiedad(Propiedad propiedad) {
+			this.propiedades.Add(propiedad);
+		}
+
 		public Propiedad ConsultarPropiedad(int número) {
 			this.propiedades.Sort();
 
@@ -25,16 +29,36 @@ namespace AlquilerLib {
 			return this.propiedades[idx];
 		}
 
-		public void AlquilarPropiedad(int nroPropiedad, Alquiler alquiler) {
-			this.ConsultarPropiedad(nroPropiedad).Alquilar(alquiler);
+		/// <summary>
+		/// Alquila una la <see cref="Propiedad"/> bajo el número indicado
+		/// </summary>
+		/// <param name="nroPropiedad"></param>
+		/// <param name="alquiler"></param>
+		/// <returns><see langword="true"/> si se pudo alquilar la Propiedad</returns>
+		public bool AlquilarPropiedad(int nroPropiedad, Alquiler alquiler) {
+			Propiedad propiedad = this.ConsultarPropiedad(nroPropiedad);
+
+			if(propiedad is null)
+				return false;
+
+			propiedad.Alquilar(alquiler);
+			return true;
 		}
 
-		public void CancelarAlquiler(int nroPropiedad, int nroAlquiler) {
-			this.ConsultarPropiedad(nroPropiedad).QuitarAlquiler(nroAlquiler);
-		}
-		public void AgregarPropiedad(Propiedad propiedad)
-		{
-			this.propiedades.Add(propiedad);
+		/// <summary>
+		/// Cancela el <see cref="Alquiler"/> bajo el número de alquiler indicado, alojado en la <see cref="Propiedad"/> bajo el número indicado
+		/// </summary>
+		/// <param name="nroPropiedad"></param>
+		/// <param name="nroAlquiler"></param>
+		/// <returns><see langword="true"/> si se pudo cancelar el Alquiler</returns>
+		public bool CancelarAlquiler(int nroPropiedad, int nroAlquiler) {
+			Propiedad propiedad = this.ConsultarPropiedad(nroPropiedad);
+
+			if(propiedad is null)
+				return false;
+
+			propiedad.QuitarAlquiler(nroAlquiler);
+			return true;
 		}
 	}
 }
