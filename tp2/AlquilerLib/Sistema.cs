5882501ffcb100150953360cb2ef3e5b99cd6618
PapitaConPure <33335038+PapitaConPure@users.noreply.github.com>
2023-10-16 21:22:45 -0300

    Documentar

--- tp2/AlquilerLib/Sistema.cs
+++ tp2/AlquilerLib/Sistema.cs
@@ -13,10 +13,19 @@ namespace AlquilerLib {
 			this.propiedades = new List<Propiedad>();
 		}
 
+		/// <summary>
+		/// Agrega una propiedad al sistema
+		/// </summary>
+		/// <param name="propiedad">propiedad a incorporar</param>
 		public void AgregarPropiedad(Propiedad propiedad) {
 			this.propiedades.Add(propiedad);
 		}
 
+		/// <summary>
+		/// Consulta una propiedad por número de propiedad
+		/// </summary>
+		/// <param name="nroPropiedad">número de propiedad a buscar</param>
+		/// <returns>La <see cref="Propiedad"/> bajo el número indicado, o <see langword="null"/> si no se encuentra</returns>
 		public Propiedad ConsultarPropiedad(int número) {
 			this.propiedades.Sort();
 
@@ -30,11 +39,11 @@ namespace AlquilerLib {
 		}
 
 		/// <summary>
-		/// Alquila una la <see cref="Propiedad"/> bajo el número indicado
+		/// Alquila la propiedad bajo el número indicado si existe
 		/// </summary>
-		/// <param name="nroPropiedad"></param>
-		/// <param name="alquiler"></param>
-		/// <returns><see langword="true"/> si se pudo alquilar la Propiedad</returns>
+		/// <param name="nroPropiedad">número de la propiedad a alquilar</param>
+		/// <param name="alquiler">alquiler a relacionar con la propiedad</param>
+		/// <returns><see langword="true"/> si se pudo alquilar la <see cref="Propiedad"/></returns>
 		public bool AlquilarPropiedad(int nroPropiedad, Alquiler alquiler) {
 			Propiedad propiedad = this.ConsultarPropiedad(nroPropiedad);
 
@@ -46,7 +55,7 @@ namespace AlquilerLib {
 		}
 
 		/// <summary>
-		/// Cancela el <see cref="Alquiler"/> bajo el número de alquiler indicado, alojado en la <see cref="Propiedad"/> bajo el número indicado
+		/// Cancela el alquiler indicado para la propiedad indicada
 		/// </summary>
 		/// <param name="nroPropiedad"></param>
 		/// <param name="nroAlquiler"></param>
